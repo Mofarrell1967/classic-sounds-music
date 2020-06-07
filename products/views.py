d1cbba32be2c35797fb67c6c9aec42fedd875113
Michael O'Farrell <mofarrell1967@gmail.com>
2020-06-07 13:39:07 +0000

    Updated pagination in products and applied admin code

--- products/views.py
+++ products/views.py
@@ -1,47 +1,86 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 """
 A view that displays all products
+Paginate is filtered on product name
 
 """
 
 
 def all_products(request):
-    products = Product.objects.all()
+    product_list = Product.objects.filter().order_by('name')
+    paginator = Paginator(product_list, 6)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "products.html", {"products": products})
 
 
 """
 A view that displays only the Guitars
+Paginate is filtered on product name
 
 """
 
 
 def guitars(request):
-    products = Product.objects.filter(category='guitars')
+    product_list = Product.objects.filter(category='guitars').order_by('name')
+    paginator = Paginator(product_list, 6)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "guitars.html", {"products": products})
 
 
 """
 A view that displays only the Keyboards
+Paginate is filtered on product name
 
 """
 
 
 def keyboards(request):
-    products = Product.objects.filter(category='keyboards')
+    product_list = Product.objects.filter(
+        category='keyboards').order_by('name')
+    paginator = Paginator(product_list, 6)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "keyboards.html", {"products": products})
 
 
 """
 A view that displays only the Percussion
+Paginate is filtered on product name
 
 """
 
 
 def percussion(request):
-    products = Product.objects.filter(category='percussion')
+    product_list = Product.objects.filter(
+        category='percussion').order_by('name')
+    paginator = Paginator(product_list, 6)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "percussion.html", {"products": products})
 
 
