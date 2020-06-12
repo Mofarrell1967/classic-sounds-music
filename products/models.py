dabbcbc9ebaeeef2ef096374322853181cd5cbfc
Michael O'Farrell <mofarrell1967@gmail.com>
2020-06-12 10:52:21 +0000

    Updated product_detail.html and models.py to display additional fields

--- products/models.py
+++ products/models.py
@@ -9,9 +9,14 @@ Model for the fields for the site products
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     category = models.CharField(max_length=25, default='')
-    description = models.TextField()
+    description_summary = models.TextField(default='')
+    model = models.CharField(max_length=50, default='')
+    description = models.TextField(default='')
+    maker = models.CharField(max_length=25, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
+    image1 = models.ImageField(upload_to='images', null=True, blank=True)
+    image2 = models.ImageField(upload_to='images', null=True, blank=True)
+    image3 = models.ImageField(upload_to='images', null=True, blank=True)
 
     def __str__(self):
         return self.name
